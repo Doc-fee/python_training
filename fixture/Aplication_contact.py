c47e3ffe2db8cdd18a18e680dd65ac78a97d80a0
Manya <bugsbunny.k38@gmail.ru>
2020-03-31 21:22:48 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестов Выделен класс для работы с контактами

--- fixture/Aplication_contact.py
+++ fixture/Aplication_contact.py
@@ -1,15 +1,13 @@
-#yo
 from selenium import webdriver
 from fixture.session import SessionHelper
-from fixture.group import GroupHelpter
+from fixture.contact import ContactHelper
 
-class Aplication:
-
+class Aplication_co:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-        self.group = GroupHelpter(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from Aplication_contact import Aplication_co
+from model.contact import Contact
+from fixture.Aplication_contact import Aplication_co
 
 
 @pytest.fixture
@@ -11,11 +11,10 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-        app.open_home_page()
-        app.login(username="admin", password="secret")
-        app.fill_contact_form(Contact(name = "Anna", middlename = "Mc", lastname = "Smith", nickname = "Ann", title = "-",company = "IT", address = "world",
+    app.session.login(username="admin", password="secret")
+    app.contact.fill_contact_form(Contact(name = "Anna", middlename = "Mc", lastname = "Smith", nickname = "Ann", title = "-",company = "IT", address = "world",
                  homephone = "5556677", mobile='87459623125', work="4182369", fax = "4569632", email="kloun@mail.ru", email2="-", email3="-", homepage="-", bday="5",
                  bmonth="January", byear="2000", address2="world", phone2="7418526", notes="-"))
-        app.submit()
-        app.return_home_page()
-        app.logout()
+    app.contact.submit()
+    app.contact.return_home_page()
+    app.session.logout()
