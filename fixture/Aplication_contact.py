1b8db432a23b26061c5d2b54358f065420933012
Manya <bugsbunny.k38@gmail.ru>
2020-04-01 18:24:23 +0300

    Поместила всех попмощников в один класс

--- fixture/Aplication_contact.py
+++ fixture/Aplication_contact.py
@@ -7,7 +7,7 @@ class Aplication_co:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-        self.contact = ContactHelper(self)
+
 
     def open_home_page(self):
         wd = self.wd
