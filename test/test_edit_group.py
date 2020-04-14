bd4cd57eb12ae7e2ab44240a446c21d27ca40a25
Manya <bugsbunny.k38@gmail.ru>
2020-04-14 20:31:34 +0300

    Реализованы проверки путём сравнения списков для всех тестов групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,11 +6,21 @@ from model.grroup import Group
 #     app.session.logout()
 
 def test_edit_group_name(app):
+    old_groups = app.group.get_group_list()
+    group = Group(name='Train')
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
-    app.group.edit_first_group(Group(name='Train'))
+    app.group.edit_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_edit_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(header='test'))
-    app.group.edit_first_group(Group(header='Submarine'))
+# def test_edit_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create(Group(header='test'))
+#     app.group.edit_first_group(Group(header='Submarine'))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
