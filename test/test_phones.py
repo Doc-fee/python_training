fb37ecf95f649f8daeb4f7c6eedd9dbf00457993
Manya <bugsbunny.k38@gmail.ru>
2020-05-01 11:25:57 +0300

    объединила тесты (проверки email, телефонов и адресов) в один тест

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,14 @@
 import re
 
-def test_phones_home_page(app):
+def test_contacts_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    # phones
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    # address
+    assert contact_from_home_page.address == contact_from_edit_page.address
+    # emails
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
@@ -14,9 +19,16 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.work == contact_from_edit_page.work
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
-def clear(s):
+def clear_phones(s):
         return re.sub("[() -]","", s)
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x), [contact.homephone, contact.mobile, contact.work, contact.phone2])))
+                            map(lambda x: clear_phones(x), [contact.homephone, contact.mobile, contact.work, contact.phone2])))
+
+def clear_emails(a):
+    return re.sub("[ ]", "", a)
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear_emails(x), [contact.email, contact.email2, contact.email3])))
