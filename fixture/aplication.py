0f24336e889c1692656bc4a1b37515fe3b96286e
Manya <bugsbunny.k38@gmail.ru>
2020-05-01 12:04:28 +0300

    Реализованы опции для указания типа браузера и адреса приложения

--- fixture/aplication.py
+++ fixture/aplication.py
@@ -6,12 +6,20 @@ from fixture.contact import ContactHelper
 
 class Aplication:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = wbdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'IE':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelpter(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,10 +31,7 @@ class Aplication:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith('addressbook/')):
-            wd.get("http://localhost:8080/addressbook/")
-
-
-
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
